90924dba6bb99587b2c5d7ae7ac964a5b9fd2703
TangB5 <ndohyannick78@gmail.com>
2025-06-23 14:20:05 +0100

    mis a jour  d un formulaire avec angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'App';
+  
+nom :string ="john ";
+prenom:string=" doe";
+age :number =25;
+description :string="un developeur web travailant et passionné par les nouvelles technologies";
+profession :string="developpeur web";
+pays :string="cameroun";
+ville :string="yaounde";
+
+
 }
